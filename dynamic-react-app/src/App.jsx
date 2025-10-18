a4db827c3ffbb7f105b0c442f4bac862fd15161f
edwardmartin1 <edwardmartin1@yahoo.com>
2025-10-18 18:45:01 -0500

    commit final change

--- dynamic-react-app/src/App.jsx
+++ dynamic-react-app/src/App.jsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-      <div className="card-grid">
+      <div>
         <Header />
         <ProfileContainer profiles={profileArray} />
       </div>
